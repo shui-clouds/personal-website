26d5970eac98e3900707867291a9071d73c2e133
Shui Shan <shui.clouds@gmail.com>
2024-05-01 11:06:36 +0100

    add main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,113 @@
+import { profile } from 'console'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import mountains from '../public/mountains.png'
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
+type ProjectCard = {
+  title: string,
+  description: string,
+  path: string,
+  borderColours: string,
 }
 
-export default Home
+type ProjectLink = {
+  title: string,
+  description: string,
+  path: string,
+}
+
+const projects: ProjectCard[] = [
+  {
+    title: 'ChatOps Platform 💬',
+    path: '/chatops',
+    description: 'Centralized and extensible ChatOps platform for system monitoring, alerting and troubleshooting',
+    borderColours: 'bg-gradient-to-tr from-[#26deff] via-[#bc35ff] to-[#fc0bf8]'
+  },
+  {
+    title: 'Customer Portal Integration',
+    path: '/feedback-management',
+    description: 'Solution for managing and tracking customer feedback and issues using internal PM tools',
+    borderColours: 'bg-gradient-to-r from-[#fc0bf8] via-[#ff195e] to-[#ff9626]'
+  },
+  {
+    title: 'AWS Private Bridge ☁️',
+    path: '/aws-bridge',
+    description: 'Secure, customizable reverse-proxy system for tunnelling traffic into internal network via signed URLs',
+    borderColours: 'bg-gradient-to-tr from-[#fcb80b] via-[#ffda35] to-[#13d409]'
+  },
+]
+
+const projectLinks: ProjectLink[] = [
+  {
+    title: 'Mangui 🏡',
+    description: 'Tenancy & Property Management web app',
+    path: '/mangui',
+  },
+  {
+    title: 'Chatty Chef 🧑‍🍳',
+    description: 'CMS-powered Restaurant website for online ordering',
+    path: '/chatty-chef',
+  },
+  {
+    title: 'Unity Game 🎮',
+    description: 'Here are the biggest enterprise technology acquisitions of 2021 so far',
+    path: '/unity-dev',
+  },
+]
+
+
+const ProjectCard = ({ project }: { project: ProjectCard }) => (
+  <Link href={project.path} passHref>
+    <a className={`p-1 bg-white rounded-xl ${project.borderColours}`}>
+      <div className='bg-white rounded-lg pt-4 pb-8 px-5 min-h-full '>
+        <p className='mb-2 text-lg font-medium text-gray-90'>{project.title}</p>
+        <p className='font-normal text-gray-700 dark:text-gray-400'>{project.description}</p>
+      </div>
+    </a>
+  </Link>
+)
+
+const ProjectLink = ({ project }: { project: ProjectLink }) => (
+  <Link href={project.path} passHref>
+    <a>
+      <h2 className='cursor-pointer text-2xl font-medium hover:text-blue-800'>{project.title}</h2>
+      <p className='text-gray-700 dark:text-gray-400'>{project.description}</p>
+    </a>
+  </Link>
+)
+
+export default function Home() {
+  return (
+    <main>
+      <div className='flex flex-col max-w-3xl mx-auto pt-8 sm:px-4 px-8 pb-8'>
+        <div className='flex flex-col-reverse sm:flex-row items-start justify-between'>
+          <div className='flex flex-col sm:pr-16'>
+            <h1 className='text-4xl sm:text-5xl font-bold'>Shui Shan 💫</h1>
+            <p className='italic mt-2 text-md text-gray-500'>apps & game dev 👾 music & animal lover 🍜</p>
+            <p className='mt-4 mb-8 text-lg'>Passionate about software engineering, pursuring challenges and collaborating with people.</p>
+            <p className='text-lg'>@shui-clouds</p>
+            <p className='text-lg'>/shuishan</p>
+          </div>
+          <div className="w-[100px] sm:w-[300px] mb-4 sm:mb-0">
+            <Image className='rounded-full' alt='Pixel Moutains' src={mountains} />
+          </div>
+        </div>
+        <h2 className='text-3xl mt-16 mb-4 font-bold'>
+          Project Highlight 🌟
+        </h2>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 mt-2 pb-4'>
+          {projects.map((project) => (
+            <ProjectCard key={project.title} project={project} />
+          ))}
+        </div >
+        <div className='flex flex-col gap-5 mt-4'>
+          {projectLinks.map((project) => (
+            <ProjectLink key={project.title} project={project} />
+          ))}
+        </div>
+      </div >
+    </main >
+  )
+}
